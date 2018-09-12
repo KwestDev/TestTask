5dcf94812f7946ef453e61a2051a1000da01d3e7
JibranAli <jibran.ali.005@gmail.com>
2018-09-12 20:08:53 +0500

    UpdateStates for animation

--- Dummy Fighter/Assets/SimulationController.cs
+++ Dummy Fighter/Assets/SimulationController.cs
@@ -12,7 +12,8 @@ public class SimulationController : MonoBehaviour {
     private List<LinkData> EnemyChain;
     public Image PlayerHealthBar;
     public Image EnemyHealthBar;
-
+    public bool playerReturn { get; set; }
+    public bool enemyReturn { get; set; }
     int indexPlayer = 0;
     int indexEnemy = 0;
     PlayerState statePlayer;
@@ -21,6 +22,8 @@ public class SimulationController : MonoBehaviour {
     void Start () {
         PlayerChain = JsonMapper.ToObject<List<LinkData>>(File.ReadAllText(Application.dataPath + "/StreamingAssets/Player.json"));
         EnemyChain  = JsonMapper.ToObject<List<LinkData>>(File.ReadAllText(Application.dataPath + "/StreamingAssets/Enemy.json"));
+        playerReturn = false;
+        enemyReturn = false;
 
 
     }
@@ -31,39 +34,153 @@ public class SimulationController : MonoBehaviour {
             return PlayerState.Attack;
         else if (chain[index].Type == LinkState.Dodge)
             return PlayerState.Dodge;
-        else
+        else if (chain[index].Type == LinkState.Watch)
+        {
+            //random
+            ChainSwapper(chain, index);
+
             return PlayerState.Idle;
+        }
+
+
+        ChainSwapper(chain, index);
+       
+        return PlayerState.Idle;
     }
+    void ChainSwapper (List<LinkData> chain , int index)
+    {
+        int swapIndex=0,propertyIndex=0;
+        bool increment = true;
+        if (index == chain.Count - 1)
+            propertyIndex = 0;
+        else
+            propertyIndex = index+1;
+
+            switch (chain[propertyIndex].Type)
+                {
+                    case LinkState.Attack:
+                        swapIndex = chain[0].Attack;
+                        break;
+                    case LinkState.Dodge:
+                        swapIndex = chain[0].Dodge;
+                        break;
+                    case LinkState.Think:
+                        swapIndex = chain[0].Idle;
+                        break;
+                    case LinkState.Watch:
+                        swapIndex = chain[0].Idle;
+                        break;
+
+                }
+
+        if (swapIndex < 0)
+        {
+            swapIndex -= 2 * swapIndex;
+            increment = false;
+        }
+        for (int i=0; i<swapIndex;  i++)
+        {
+            if (increment)
+            {
+                if (propertyIndex == chain.Count - 1)
+                    propertyIndex = 0;
+                else
+                    propertyIndex++;
+            }
+            else
+            {
+                if (propertyIndex == 0)
+                    propertyIndex = chain.Count-1;
+                else
+                    propertyIndex--;
+            }
+
+        }
+        swapLinks( chain, propertyIndex, index + 1 );
+
+        
+        
+    }
+
+    void swapLinks (List<LinkData> chain, int index1, int index2)
+    {
+        LinkData temp = chain[index1];
+        chain[index1] = chain[index2];
+        chain[index2] = temp; 
+    }
+
+    void updateStates()
+    {
+        if (playerReturn)
+        {
+            playerReturn = false;
+            if (statePlayer == PlayerState.Attack)
+                statePlayer = PlayerState.AttackReturn;
+            else
+                statePlayer = PlayerState.DodgeReturn;
+        }
+        else
+        {
+            if (indexPlayer == PlayerChain.Count - 1)
+            {
+
+
+                indexPlayer = 0;
+                statePlayer = ReadChain(PlayerChain, indexPlayer);
+                indexPlayer++;
+
+            }
+
+            else
+            {
+                statePlayer = ReadChain(PlayerChain, indexPlayer);
+                indexPlayer++;
+            }
+
+            if (statePlayer == PlayerState.Attack || statePlayer == PlayerState.Dodge)
+                playerReturn = true;
+        }
+
+
+        if (enemyReturn)
+        {
+            enemyReturn = false;
+            if (stateEnemy == PlayerState.Attack)
+                stateEnemy = PlayerState.AttackReturn;
+            else
+                stateEnemy = PlayerState.DodgeReturn;
+        }
+
+        else
+        {
+            if (indexEnemy == EnemyChain.Count - 1)
+            {
+
+
+                indexEnemy = 0;
+                stateEnemy = ReadChain(EnemyChain, indexEnemy);
+                indexEnemy++;
+
+            }
+
+            else
+            {
+                stateEnemy = ReadChain(EnemyChain, indexEnemy);
+                indexEnemy++;
+            }
+
+            if (stateEnemy == PlayerState.Attack || stateEnemy == PlayerState.Dodge)
+                enemyReturn = true;
+        }
+
+
+    }
+
     void Update ()
     {
-        /*
-        if ((PlayerChain[indexPlayer] == null))
-        {
-            indexPlayer = 0;
-            statePlayer = ReadChain(PlayerChain, indexPlayer);
-            indexPlayer++;
 
-        }
-        else
-        {
-            statePlayer = ReadChain(PlayerChain, indexPlayer);
-            indexPlayer++;
-        }
-
-        if ((EnemyChain[indexEnemy] == null))
-        {
-            indexEnemy = 0;
-            stateEnemy = ReadChain(EnemyChain, indexEnemy);
-            indexEnemy++;
-
-        }
-        else
-        {
-            stateEnemy = ReadChain(EnemyChain, indexEnemy);
-            indexEnemy++;
-        }
-
-        */
+       
+        
 
     }
 
